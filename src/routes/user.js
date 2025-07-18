ddb63ff6fffc15b653ce9f96e5171d156d18f9df
Atharva251 <patharva266@gmail.com>
2025-07-18 16:25:14 +0530

    Changes Made:
    - Added feed API for user feed with pagination and limit.

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const userRouter = express.Router();
 const ConnectionRequest = require('../model/connectionRequest');
+const User = require('../model/user');
 const { userAuth } = require("../middlewares/auth");
+const { set } = require('mongoose');
 
 USER_SAFE_DATA = "firstName lastName photoUrl age gender about skils";
 
@@ -43,5 +45,44 @@ userRouter.get("/user/requests/connections", userAuth, async (req, res) => {
         res.status(400).send("Something went wrong: " + err.message);
     }
 })
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+        const loggedInUser = req.user;
+
+        const userConnections = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id, },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUsersFromFeed = new Set();
+        userConnections.forEach((row) => {
+            if (row.fromUserId.equals(loggedInUser._id)) {
+                hideUsersFromFeed.add(row.toUserId.toString());
+            } else {
+                hideUsersFromFeed.add(row.fromUserId.toString());
+            }
+        });
+
+        const feedData = await User.find({
+            $and: [
+                { _id: { $ne: loggedInUser._id } },
+                { _id: { $nin: Array.from(hideUsersFromFeed) } }
+            ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        res.status(200).json({ data: feedData });
+    } catch (err) {
+        res.status(400).send("Something went wrong: " + err.message);
+    }
+});
 
 module.exports = userRouter;
